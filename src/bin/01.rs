f096f3b613e4cc9f762d79c55b04be6fa5ba0c9d
Misael Cureño <curenosm@gmail.com>
2023-12-01 12:55:30 -0600

    .

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -8,22 +8,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn obtain_number(input: &str) -> u32 {
+    let l = input.chars().find(|&c| c.is_digit(10)).unwrap().to_digit(10);
+    let r = input.chars().rfind(|&c| c.is_digit(10)).unwrap().to_digit(10);
 
-    let digit1 = input.chars().find(|&c| c.is_digit(10)).unwrap().to_digit(10);
-    let digit2 = input.chars().rfind(|&c| c.is_digit(10)).unwrap().to_digit(10);
-
-    if let (Some(digit1), Some(digit2)) = (digit1, digit2) {
-        return digit1 * 10 + digit2;
+    if let (Some(l), Some(r)) = (l, r) {
+        return l * 10 + r;
     }
 
     0
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    Some(input
-        .split("\n")
-        .map(|line| obtain_number(line))
-        .reduce(|acc, number| acc + number)?)
+pub fn part_two(_input: &str) -> Option<u32> {
+    part_one(_input)
 }
 
 #[cfg(test)]
@@ -33,12 +29,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 142);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 142);
+        assert_eq!(result, Some(142));
     }
 }
