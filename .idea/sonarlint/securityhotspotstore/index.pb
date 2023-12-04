289c7dd2d0926d0f63c1ba7ff4c2230b95bcebec
Misael Cureño <curenosm@gmail.com>
2023-12-04 11:14:44 -0600

    Avances en el modelado de los datos del día 4

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,21 +0,0 @@
-
-B
-data/inputs/02.txt,c\c\cc81423abccde55adcf2758a6053433a87c214ed
-D
-data/examples/01.txt,3\2\32de0d84433fc6065d7e9b69998f70cc8ce8796d
-C
-src/template/mod.rs,c\e\ce7138d988288aaa4294a9cf5a74b94764db62cc
-B
-data/inputs/01.txt,d\8\d8500880b4f6ea8bd2eeb0d9ba0494e3d3f286ba
-B
-data/puzzles/01.md,5\4\5444af81b14fc3d9b3dd6cac584383239394faad
-=
-src/bin/02.rs,4\3\43c9176e6eb4eba7a1b43c9a479149e2250c57c0
-9
-	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-=
-src/bin/01.rs,9\e\9e3811e438d28bebc22bb15247eeecb33b833f00
-D
-data/examples/02.txt,d\d\dd49a10853aff5952c1332236939f5df7fea1a92
-B
-data/puzzles/02.md,2\3\231a479c67c463115229f2e03d693e06f9865da2
---
+++ src/bin/04.rs
@@ -0,0 +1,103 @@
+advent_of_code::solution!(4);
+
+#[derive(Debug)]
+pub struct Card {
+    id: u32,
+    own_numbers: [u32; 5],
+    winner_numbers: [u32; 8]
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let cards = input
+        .trim()
+        .split('\n')
+        .map(clean_row)
+        .map(parse_card)
+        .collect::<Vec<Card>>();
+
+    print_cards(&cards);
+
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn clean_row(row: &str) -> &str {
+    if row.ends_with('\n') || row.ends_with('\r') {
+        row[..row.len() - 1].trim()
+    } else {
+        row.trim()
+    }
+}
+
+pub fn parse_card(card_str: &str) -> Card {
+    let mut own_numbers = [0; 5];
+    let mut winner_numbers = [0; 8];
+    let mut split_line = card_str
+        .split(':')
+        .map(clean_row);
+    let mut card_part = split_line
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|s| !s.is_empty());
+    let id = card_part
+        .nth(1)
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    let mut numbers_part = split_line
+        .next()
+        .unwrap()
+        .trim()
+        .split('|')
+        .map(clean_row);
+    let own = parse_number_list(numbers_part.next().unwrap());
+    let winners = parse_number_list(numbers_part.next().unwrap());
+
+    for i in 0..8 {
+        if i < 5 {
+            own_numbers[i] = own[i];
+        }
+        winner_numbers[i] = winners[i];
+    }
+
+    Card { id, own_numbers, winner_numbers}
+}
+
+pub fn parse_number_list(numbers_str: &str) -> Vec<u32> {
+    numbers_str
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(clean_row)
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+}
+
+pub fn print_cards(cards: &[Card]) {
+    cards.iter().for_each(|card| {
+        println!("Card {}:", card.id);
+        println!("\tOwn numbers: {:?}", card.own_numbers);
+        println!("\tWinner numbers: {:?}", card.winner_numbers);
+        println!();
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, 13);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, 13);
+    }
+}
