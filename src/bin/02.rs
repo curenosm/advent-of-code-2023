e1c4cb73bb9622bc26642f3dbd942bdc78fd00f7
misacs <misael.cureno@soltec.mx>
2023-12-24 11:13:23 -0600

    descargando retos pendientes

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -76,12 +76,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 8);
+        assert_eq!(result, Some(3059));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 8);
+        assert_eq!(result, Some(3059));
     }
 }
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -99,12 +99,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 35);
+        assert_eq!(result, Some(35));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, 35);
+        assert_eq!(result, Some(35));
     }
 }
